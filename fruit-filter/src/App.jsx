4dca8ed04dc2c704dedef5e8caee18451ee85794
Nikhil Gangajaliya <nikhilgangajaliya5@gmail.com>
2026-03-26 11:52:29 +0530

    applied major css

--- fruit-filter/src/App.jsx
+++ fruit-filter/src/App.jsx
@@ -39,7 +39,24 @@ function App() {
     <>
       <div className="container">
         <div className="content">
-          <h1>Fruit-Filter App</h1>
+          <h1 class="heading">
+            <span>F</span>
+            <span>r</span>
+            <span>u</span>
+            <span>i</span>
+            <span>t</span>
+            <span>-</span>
+            <span>F</span>
+            <span>i</span>
+            <span>l</span>
+            <span>t</span>
+            <span>e</span>
+            <span>r</span>
+            <span> </span>
+            <span>A</span>
+            <span>p</span>
+            <span>p</span>
+          </h1>
 
           <input type="text" placeholder="Search the fruit" value={filter} onChange={handleFilterChange} />
 
